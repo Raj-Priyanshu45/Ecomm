99bd461528c1da21a54a0c172851bab076de804f
Aayush Raj <devxpriyanshu@gmail.com>
2026-03-27 13:35:06 +0530

    p

--- my-app/src/app/config/auth.config.ts
+++ my-app/src/app/config/auth.config.ts
@@ -1,10 +1,17 @@
 import { PassedInitialConfig } from 'angular-auth-oidc-client';
 
+const getBaseUrl = () => {
+  if (typeof window !== 'undefined') {
+    return window.location.origin;
+  }
+  return 'http://localhost:4200'; // fallback for SSR
+};
+
 export const authConfig: PassedInitialConfig = {
   config: {
     authority: 'http://localhost:8181/realms/ecomApp',
-    redirectUrl: window.location.origin,
-    postLogoutRedirectUri: window.location.origin,
+    redirectUrl: getBaseUrl(),
+    postLogoutRedirectUri: getBaseUrl(),
     clientId: 'angular-client',
     scope: 'openid profile offline_access',
     responseType: 'code',
